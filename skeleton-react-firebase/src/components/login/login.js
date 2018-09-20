4eb30ee22e685ba8950878d3cb6ed8647d5511f1
Jorge Guerra <jorgeguerrask8@gmail.com>
2018-09-20 09:48:02 -0600

    Lil changes

---
+++ skeleton-react-firebase/src/components/login/login.js
@@ -0,0 +1,4 @@
+//https://engineering.musefind.com/our-best-practices-for-writing-react-components-dec3eb5c3fc8
+
+//import { auth } from '../../firebase';
+//import * as routes from '../../constants/routes';
